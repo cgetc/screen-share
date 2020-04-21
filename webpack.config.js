6cd4ba092111991d625f33e191c4bd98d5ce88ee
cgetc <cgetc502@gmail.com>
2020-04-21 21:27:07 +0900

    html to template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
@@ -10,7 +11,7 @@ module.exports = {
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: path.join(__dirname, 'public', 'dist'),
+    path: path.join(__dirname, 'dist'),
     // 出力ファイル名
     filename: "main.js"
   },
@@ -24,6 +25,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: 'src/templates/index.html',
+      publicPath: '/',
+    })
+  ],
   // import 文で .ts ファイルを解決するため
   // これを定義しないと import 文で拡張子を書く必要が生まれる。
   // フロントエンドの開発では拡張子を省略することが多いので、
@@ -35,6 +43,6 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    compress: true,
   },
 };
