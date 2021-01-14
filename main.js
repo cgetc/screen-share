c32b0562938df13d3677d896844bb9eedd393a8c
cgetc <1819135+cgetc@users.noreply.github.com>
2021-01-14 12:12:57 +0000

    Deploying to gh-pages from  @ 969480337946bba36e267e262cd1e21e990a3e08 🚀

--- main.js
+++ main.js
@@ -81,42 +81,19 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/main.ts-exposed");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/main.ts");
 /******/ })
 /************************************************************************/
 /******/ ({
-
-/***/ "./node_modules/webpack/buildin/global.js":
-/*!***********************************!*\
-  !*** (webpack)/buildin/global.js ***!
-  \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn this;\n})();\n\ntry {\n\t// This works if eval is allowed (see CSP)\n\tg = g || new Function(\"return this\")();\n} catch (e) {\n\t// This works if the window reference is available\n\tif (typeof window === \"object\") g = window;\n}\n\n// g can still be undefined, but nothing to do about it...\n// We return undefined, instead of nothing here, so it's\n// easier to handle this case. if(!global) { ...}\n\nmodule.exports = g;\n\n\n//# sourceURL=webpack:///(webpack)/buildin/global.js?");
-
-/***/ }),
 
 /***/ "./src/main.ts":
 /*!*********************!*\
   !*** ./src/main.ts ***!
   \*********************/
-/*! exports provided: init */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"init\", function() { return init; });\nconst init = (config) => {\n    console.log(config)\n}\n\n\n//# sourceURL=webpack:///./src/main.ts?");
-
-/***/ }),
-
-/***/ "./src/main.ts-exposed":
-/*!*****************************!*\
-  !*** ./src/main.ts-exposed ***!
-  \*****************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-eval("/* WEBPACK VAR INJECTION */(function(global) {module.exports = global[\"main\"] = __webpack_require__(/*! -!./main.ts */ \"./src/main.ts\");\n\n/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/global.js */ \"./node_modules/webpack/buildin/global.js\")))\n\n//# sourceURL=webpack:///./src/main.ts-exposed?");
+eval("console.log('hello');\n\n\n//# sourceURL=webpack:///./src/main.ts?");
 
 /***/ })
 
